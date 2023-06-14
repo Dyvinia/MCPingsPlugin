248ebca7d1362b07babbad5aec4d2a40134809bc
Dyvine57 <dulana57@gmail.com>
2023-06-14 19:16:11 -0400

    separate modded players from vanilla players

--- src/main/java/net/dyvinia/mcpingsplugin/MCPingsPlugin.java
+++ src/main/java/net/dyvinia/mcpingsplugin/MCPingsPlugin.java
@@ -1,19 +1,28 @@
 package net.dyvinia.mcpingsplugin;
 
+import com.google.common.io.ByteArrayDataInput;
+import com.google.common.io.ByteStreams;
 import org.bukkit.Bukkit;
+import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.plugin.messaging.PluginMessageListener;
 
+import java.util.*;
+
 public final class MCPingsPlugin extends JavaPlugin implements PluginMessageListener {
+    public final String C2S_JOIN = "mcpings-c2s:join";
 
     public final String C2S_PING = "mcpings-c2s:ping";
     public final String S2C_PING = "mcpings-s2c:ping";
+
+    private List<Player> moddedPlayers = new ArrayList<>();
 
     @Override
     public void onEnable() {
         getLogger().info("MCPings Server Init");
 
+        this.getServer().getMessenger().registerIncomingPluginChannel(this, C2S_JOIN, this);
         this.getServer().getMessenger().registerIncomingPluginChannel(this, C2S_PING, this);
         this.getServer().getMessenger().registerOutgoingPluginChannel(this, S2C_PING);
     }
@@ -26,9 +35,25 @@ public final class MCPingsPlugin extends JavaPlugin implements PluginMessageList
 
     @Override
     public void onPluginMessageReceived(String channel, Player player, byte[] message) {
-        if (channel.equals(C2S_PING)) {
+        if (channel.equals(C2S_JOIN)) {
+            if (!moddedPlayers.contains(player)) {
+                moddedPlayers.add(player);
+            }
+        }
+        else if (channel.equals(C2S_PING)) {
             for (Player p : Bukkit.getOnlinePlayers()) {
-                p.sendPluginMessage(this, S2C_PING, message);
+                if (moddedPlayers.contains(p)) {
+                    p.sendPluginMessage(this, S2C_PING, message);
+                }
+                else {
+                    ByteArrayDataInput in = ByteStreams.newDataInput(message);
+
+                    Location loc = new Location(p.getWorld(), in.readDouble(), in.readDouble(), in.readDouble());
+
+                    // handle ping for vanilla players somehow
+
+                    //Bukkit.getScheduler().runTaskLater(this, () -> something, 20L * 10);
+                }
             }
         }
     }
