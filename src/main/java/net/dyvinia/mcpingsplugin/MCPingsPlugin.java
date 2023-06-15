cfe21a40aa277ebc3c618bd05775e53bdcf3d9c7
Dyvine57 <dulana57@gmail.com>
2023-06-14 20:26:08 -0400

    Basic text display

--- src/main/java/net/dyvinia/mcpingsplugin/MCPingsPlugin.java
+++ src/main/java/net/dyvinia/mcpingsplugin/MCPingsPlugin.java
@@ -1,14 +1,24 @@
 package net.dyvinia.mcpingsplugin;
 
+import com.comphenix.protocol.PacketType;
+import com.comphenix.protocol.ProtocolLibrary;
+import com.comphenix.protocol.ProtocolManager;
+import com.comphenix.protocol.events.PacketContainer;
+import com.comphenix.protocol.reflect.StructureModifier;
+import com.comphenix.protocol.wrappers.WrappedChatComponent;
+import com.comphenix.protocol.wrappers.WrappedDataValue;
+import com.comphenix.protocol.wrappers.WrappedDataWatcher;
 import com.google.common.io.ByteArrayDataInput;
 import com.google.common.io.ByteStreams;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
+import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.plugin.messaging.PluginMessageListener;
 
 import java.util.*;
+import java.util.concurrent.ThreadLocalRandom;
 
 public final class MCPingsPlugin extends JavaPlugin implements PluginMessageListener {
     public final String C2S_JOIN = "mcpings-c2s:join";
@@ -50,11 +60,77 @@ public final class MCPingsPlugin extends JavaPlugin implements PluginMessageList
 
                     Location loc = new Location(p.getWorld(), in.readDouble(), in.readDouble(), in.readDouble());
 
-                    // handle ping for vanilla players somehow
+                    String text = in.readUTF();
 
-                    //Bukkit.getScheduler().runTaskLater(this, () -> something, 20L * 10);
+                    newTextDisplay(p, loc, 5, text);
                 }
             }
         }
+    }
+
+    private void newTextDisplay(Player player, Location loc, int duration, String text) {
+        ProtocolManager protocolManager = ProtocolLibrary.getProtocolManager();
+
+        int entId = ThreadLocalRandom.current().nextInt();
+        UUID entUuid = UUID.randomUUID();
+
+        PacketType type = PacketType.Play.Server.SPAWN_ENTITY;
+        PacketContainer packet = ProtocolLibrary.getProtocolManager().createPacket(type);
+
+        StructureModifier<Integer> intMod = packet.getIntegers();
+        StructureModifier<EntityType> typeMod = packet.getEntityTypeModifier();
+        StructureModifier<UUID> uuidMod = packet.getUUIDs();
+        StructureModifier<Double> doubleMod = packet.getDoubles();
+
+        // Write id of entity
+        intMod.write(0, entId);
+
+        // Write type of entity
+        typeMod.write(0, EntityType.TEXT_DISPLAY);
+
+        // Write entities UUID
+        uuidMod.write(0, entUuid);
+
+        // Write position
+        doubleMod.write(0, loc.getX());
+        doubleMod.write(1, loc.getY());
+        doubleMod.write(2, loc.getZ());
+
+        protocolManager.sendServerPacket(player, packet);
+
+        PacketType typeMeta = PacketType.Play.Server.ENTITY_METADATA;
+        PacketContainer packetMeta = ProtocolLibrary.getProtocolManager().createPacket(typeMeta);
+
+        packetMeta.getIntegers().write(0, entId);
+
+        WrappedDataWatcher.Serializer byteSerializer = WrappedDataWatcher.Registry.get(Byte.class);
+        WrappedDataWatcher.Serializer chatSerializer = WrappedDataWatcher.Registry.getChatComponentSerializer();
+        WrappedDataWatcher.Serializer boolSerializer = WrappedDataWatcher.Registry.get(Boolean.class);
+
+        List<WrappedDataValue> dataValues = new ArrayList<>();
+
+        Byte billboard = 3;
+        dataValues.add(new WrappedDataValue(14, byteSerializer, billboard));
+
+        Object optChat = WrappedChatComponent.fromChatMessage(text)[0].getHandle();
+        dataValues.add(new WrappedDataValue(22, chatSerializer, optChat));
+
+        Byte seeThrough = 0x02;
+        dataValues.add(new WrappedDataValue(26, byteSerializer, seeThrough));
+
+        packetMeta.getDataValueCollectionModifier().write(0, dataValues);
+
+        protocolManager.sendServerPacket(player, packetMeta);
+
+        Bukkit.getScheduler().runTaskLater(this, () -> {
+            PacketType typeKill = PacketType.Play.Server.ENTITY_DESTROY;
+            PacketContainer packetKill = ProtocolLibrary.getProtocolManager().createPacket(typeKill);
+
+            List<Integer> intList = new ArrayList<>();
+            intList.add(entId);
+
+            packetKill.getIntLists().write(0, intList);
+            protocolManager.sendServerPacket(player, packetKill);
+        }, 20L * duration);
     }
 }
